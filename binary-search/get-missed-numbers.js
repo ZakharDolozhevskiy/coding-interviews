5158f6f40ce013fc92740ee1b481098c7b7c208a
zdolozhevskiy <zakhar.dolozhevskyi@pipedrive.com>
2018-06-15 20:28:19 +0300

    update get-missed-number task

--- binary-search/get-missed-numbers.js
+++ binary-search/get-missed-numbers.js
@@ -1,53 +1,48 @@
 /*
 You have a sorted array of integers from 1 to n.
-Each number occurs exactly once but one number is missing. Write a function that accepts an array and returns the missing number.
+Each number occurs exactly once but one number is missing.
+Write a function that accepts an array and returns the missing number.
 
 Example: Array: [1, 3, 4, 5, 6] Result: 2
 
 [0, 1, 2, 3, 4]
 [1, 3, 4, 5, 6]
+
+
+1. [1, 2, 3, 5, 7, 8, 9] // 1 on the left side and 1 on the right
+2. [1, 2, 5, 6, 7, 8, 9 ] // 2 on the left side
+3. [1, 2, 3, 4, 5, 8, 9 ] // 2 on the right side
+
 */
 
-const getMissedNumber = (array, start = 0, end = array.length - 1, shift) => {
-  let mid;
-
-  if (array[end] - shift === end)
-    return array[end] + 1;
-
-  while (start < end) {
-    mid = Math.floor((start + end) / 2);
-
-    if (mid + shift === array[mid])
-      start = mid + 1;
-    else
-      end = mid;
-  }
-
-  return (array[mid] + 1 === array[mid + 1]) ? array[mid] - 1 : array[mid] + 1;
-};
-
-// Second part: write function to find 2 missed numbers in sorted list
-
-const getAllMissedNumbers = (array, start = 0, end = array.length - 1) => {
-  let mid = Math.floor((start + end) / 2);
-
-  if (array[end] - 1 === end) {
-    return [array[end] + 1, array[end] + 2];
-  }
-
+const search = (nums, shift, start = 0, end = nums.length - 1) => {
   if (start === end) {
-    return (array[mid] - 1 === array[mid - 1])
-      ? [array[mid] + 1, array[mid] + 2]
-      : [array[mid] - 2, array[mid] - 1];
+    return start + shift;
   }
 
-  if (mid + 1 === array[mid])
-    return getAllMissedNumbers(array, mid + 1, end);
-  else if (mid + 3 === array[mid])
-    return getAllMissedNumbers(array, start, mid);
-  else
-    return [
-      getMissedNumber(array, start, mid, 1),
-      getMissedNumber(array, mid, end, 2)
-    ];
+  const mid = start + ( end - start ) / 2 ^ 0;
+
+  return nums[mid] === mid + shift
+    ? search(nums, shift, mid + 1, end)
+    : search(nums, shift, start, mid);
 };
+
+const searchForAll = (nums, start = 0, end = nums.length - 1) => {
+
+  const mid = start + ( end - start ) / 2 ^ 0;
+
+  if (nums[mid] - 1 === mid) {
+    return searchForAll(nums, mid + 1, end);
+  } else if (nums[mid] - 3 === mid) {
+    return searchForAll(nums, start, mid);
+  } else {
+    return [
+      search(nums, 1, start, mid),
+      search(nums, 2, mid + 1, end)
+    ];
+  }
+};
+
+
+
+console.log(searchForAll([ 2, 3, 4, 5, 6, 7,  9]));
